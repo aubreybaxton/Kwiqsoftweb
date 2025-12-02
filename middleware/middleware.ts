43483a497edaf1e92d4440a8543d86b0c41dbd0d
Aubrey Baxton <aubrey.dev@pm.me>
2025-12-03 00:13:04 +0530

    working on finishing for home  and admin page

---
+++ middleware/middleware.ts
@@ -0,0 +1,25 @@
+// ```
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export function middleware(req: NextRequest) {
+//   const { nextUrl } = req;
+//   const protectedRoutes = ["/account"];
+//   const isProtectedRoute = protectedRoutes.some((route) =>
+//     nextUrl.pathname.startsWith(route)
+//   );
+//   if (isProtectedRoute) {
+//     const token = req.cookies.get("arb-token")?.value;
+//     if (!token) {
+//       if (nextUrl.pathname !== "/") {
+//         return NextResponse.redirect(new URL("/", req.url));
+//       }
+//     }
+//   }
+//   return NextResponse.next();
+// }
+// export const config = {
+//   matcher: ["/account/:path*"],
+// };
+
+// ```
