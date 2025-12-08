7f53778a0ede2ac3bfeb04bd543eea50474537a1
Aubrey Baxton <bg548@protonmail.com>
2025-12-09 00:30:32 +0530

    added react hook form and login ui

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Mail,Lock } from 'lucide-react';
+
 
 type Inputs={
   email:string,
@@ -28,25 +32,15 @@ function Admin() {
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)} action="#">
                 <div>
-                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <label htmlFor="email" className="flex items-center mb-2 text-sm font-medium text-gray-900 "><Mail /> Email</label>
                   <input type="email" id="email" {...register('email')} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" />
                 </div>
                 <div>
-                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 "> <Lock />Password</label>
                   <input type="password"  id="password" {...register('password')} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2" />
                 </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label htmlFor="remember" className="text-gray-500 ">Remember me</label>
-                    </div>
-                  </div>
-                  <Link href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</Link>
-                </div>
-                <button type="submit" className="w-full border text-black bg-primary-500 hover:bg-primary-700  focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                
+                <button type="submit" className="w-full border text-black bg-purple-500 hover:bg-purple-700 rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                 
               </form>
             </div>
